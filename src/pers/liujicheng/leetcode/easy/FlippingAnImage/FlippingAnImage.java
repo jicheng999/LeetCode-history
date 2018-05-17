2f448ea1877ed1b2784c80a339c7a46293e9de19
jichengliu <jicheng999@aliyun.com>
2018-05-18 00:36:27 +0800

    1.flipping an image

---
+++ src/pers/liujicheng/leetcode/easy/FlippingAnImage/FlippingAnImage.java
@@ -0,0 +1,51 @@
+package pers.liujicheng.leetcode.easy.FlippingAnImage;
+
+/**
+ * Created by JC on 2018/5/17.
+ *
+ *
+ Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+
+ To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+ To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+ Example 1:
+
+ Input: [[1,1,0],[1,0,1],[0,0,0]]
+ Output: [[1,0,0],[0,1,0],[1,1,1]]
+ Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ Example 2:
+
+ Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+ Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ Notes:
+
+ 1 <= A.length = A[0].length <= 20
+ 0 <= A[i][j] <= 1
+ */
+public class FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            flipHorizontally(A[i]);
+        }
+        return A;
+    }
+
+    public int[] flipHorizontally(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        int temp=0;
+        while(start<=end){
+            temp=arr[start];
+            arr[start]=arr[end]^1;
+            arr[end]=temp^1;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+}
--- src/pers/liujicheng/leetcode/launch/Launch.java
+++ src/pers/liujicheng/leetcode/launch/Launch.java
@@ -1,15 +1,23 @@
 package pers.liujicheng.leetcode.launch;
 
-import pers.liujicheng.leetcode.easy.missingnumber.MissingNumber;
+
+import pers.liujicheng.leetcode.easy.FlippingAnImage.FlippingAnImage;
 
 public class Launch {
-	public static void main(String[] args) {
-		MissingNumber entry=new MissingNumber();
-		int[] arr={3,0,1};
-		int result=0;
-		long start=System.currentTimeMillis();
-		result=entry.missingNumber(arr);
-		System.out.println(System.currentTimeMillis()-start);
-		System.out.println(entry.missingNumber(arr)); 
-	}
+    public static void main(String[] args) {
+        FlippingAnImage entry = new FlippingAnImage();
+        int[][] arr = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        int[][] result;
+        long start = System.currentTimeMillis();
+        result = entry.flipAndInvertImage(arr);
+        System.out.println(System.currentTimeMillis() - start);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println("");
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j]);
+            }
+        }
+    }
+
 }
