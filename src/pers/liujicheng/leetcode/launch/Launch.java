53f90bec4c4df9da5c4f52a4190af998cfdd9133
jichengliu <jicheng999@aliyun.com>
2018-05-19 01:49:16 +0800

    1.hamming distance

--- src/pers/liujicheng/leetcode/launch/Launch.java
+++ src/pers/liujicheng/leetcode/launch/Launch.java
@@ -1,14 +1,13 @@
 package pers.liujicheng.leetcode.launch;
 
+import pers.liujicheng.leetcode.easy.hammingdistance.HammingDistance;
 import pers.liujicheng.leetcode.easy.niqueorseodeWords.UniqueMorseCodeWords;
 
 public class Launch {
     public static void main(String[] args) {
-        UniqueMorseCodeWords entry = new UniqueMorseCodeWords();
-        String[] arr = {"gin", "zen", "gig", "msg"};
-        long start = System.currentTimeMillis();
-        int result=entry.uniqueMorseRepresentations(arr);
-        System.out.println(System.currentTimeMillis() - start);
+        HammingDistance entry = new HammingDistance();
+        int x=73,y=93;
+        int result=entry.hammingDistance(x,y);
         System.out.println("result:"+result);
     }
 
